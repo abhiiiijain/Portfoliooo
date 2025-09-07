8e4784c96aa3e33118a7365811ca76e688d3eed5
abhiiiijain <abhinandanbansal123@gmail.com>
2025-09-07 21:14:40 +0530

    Revert "build added"
    This reverts commit 6ac3904383a3c5b6f8d5823c5be9851e2250633e.

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
-const isGithubPages = process.env.GITHUB_PAGES === 'true';
-const repoName = 'Portfoliooo';
-
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  assetPrefix: isGithubPages ? `/${repoName}/` : undefined,
-  basePath: isGithubPages ? `/${repoName}` : undefined,
 };
 
 module.exports = nextConfig;
